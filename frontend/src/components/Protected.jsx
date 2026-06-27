c2425f9ee62b605ec4796138e33427b94d07eea2
anand raj <git add README.md>
2026-06-27 09:37:58 +0530

    bug 4 in repolens

--- frontend/src/components/Protected.jsx
+++ frontend/src/components/Protected.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Navigate } from 'react-router-dom'; // 💡 Added Navigate here
 
-const Protected = (children) => {
-    const loading = useSelector(state=>state.auth.loading)
-    const authenticated = useSelector(state=>state.auth.isAuthenticated)
+const Protected = ({ children }) => { // 💡 Added curly braces here
+    const loading = useSelector(state => state.auth.loading)
+    const authenticated = useSelector(state => state.auth.isAuthenticated)
     const navigate = useNavigate()
-if (loading) {
-  return <h1>Loading...</h1>;
-}
 
-if (!authenticated) {
-  return <Navigate to="/login" replace />;
-}
+    if (loading) {
+        return <h1>Loading...</h1>;
+    }
 
-  return  children
+    if (!authenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return children
 }
 
 export default Protected
